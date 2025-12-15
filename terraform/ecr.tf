3221a4d527ee7462d845b320f3e17fac2530ada6
Ilan Bowen <ilanbowen@gmail.com>
2025-12-15 02:31:24 +0200

    updated deploy condition

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -22,7 +22,7 @@ resource "aws_ecr_repository" "app" {
 
 # ECR Repository for Helm charts (OCI format)
 resource "aws_ecr_repository" "helm" {
-  name                 = "${var.project_name}-helm/${var.project_name}-app"
+  name                 = "${var.project_name}-helm"
   image_tag_mutability = "MUTABLE"
 
   image_scanning_configuration {
